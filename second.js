a7bf682ae0d9fb606dfcffafea1e4b17692274d7
mauriciomueller <mauriciomueller@gmail.com>
2023-04-21 10:35:00 -0300

    Second exercise refactoring

--- second.js
+++ second.js
@@ -1,11 +1,10 @@
-array = [-10, -5, 0, 5, 10]
+function sortedSquaredArray(array) {
+  const squares = array.map(item => item ** 2);
+  squares.sort((a, b) => a - b);
+  
+  return squares;
+}
 
-array.sort()
-
-console.log(array)
-
-const squares = array.map(item => {
- return item*item;
-});
-
-console.log(squares);
+console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])); // [1, 4, 9, 25, 36, 64, 81]
+console.log(sortedSquaredArray([-2, -1])); // [1, 4]
+console.log(sortedSquaredArray([-10, -5, 0, 5, 10])); //  [0, 25, 25, 100, 100]
